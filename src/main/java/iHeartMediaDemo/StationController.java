5830fc658216ef5ce653a86eb2f6fdf4b7bfa06c
Gabe <gabewilson42@gmail.com>
2017-11-05 21:11:06 -0600

    Cleaning up my controller

--- src/main/java/iHeartMediaDemo/StationController.java
+++ src/main/java/iHeartMediaDemo/StationController.java
@@ -11,27 +11,7 @@ public class StationController {
 
     @Autowired
     private StationService stationService;
-
-    @RequestMapping("/")
-    public String index() {
-        return "Bare Bones Route Test\n";
-    }
-
-    @GetMapping("/stations")
-    public List<StationModel>GetAllStations(){
-        return null;
-    }
-
-    @GetMapping("/stations/{stationId}")
-    public List<StationModel>GetStationById(){
-        return null;
-    }
-
-    @GetMapping("/stations/{stationName}")
-    public List<StationModel>GetStationByName(){
-        return null;
-    }
-
+    
     @PutMapping("/stations/addStation")
     public StationModel addStation(@RequestBody StationModel stationModel){
         //just a test for now
@@ -42,6 +22,21 @@ public class StationController {
     public StationModel updateStation(@RequestBody StationModel stationModel){
         //just a test for now
         return stationModel;
+    }
+
+    @GetMapping("/stations")
+    public List<StationModel>GetAllStations(){
+        return null;
+    }
+
+    @GetMapping("/stations/{stationId}")
+    public StationModel GetStationById(){
+        return null;
+    }
+
+    @GetMapping("/stations/{stationName}")
+    public StationModel GetStationByName(){
+        return null;
     }
 
     @PostMapping("stations/removeStation")
